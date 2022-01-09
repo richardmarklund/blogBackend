664640013c36abbf37233b605e8a778688bb03ba
Richard Marklund <richard.marklund@gmail.com>
2022-01-09 16:17:54 +0100

    add firebase support

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import express from 'express'
+import express from "express";
+import { getAllPosts, addPost } from "./firebase.js";
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.get("/allPosts", (req, res) => {
+  getAllPosts()
+    .then((posts) => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(posts));
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500);
+      res.render("error", { error: e });
+    });
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post("/post", (req, res) => {
+  const body = req.body;
+  addPost(body)
+    .then((posts) => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(posts));
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500);
+      res.render("error", { error: e });
+    });
+});
 
-app.listen(port)
+app.listen(port);
---
+++ firebase.js
@@ -0,0 +1,29 @@
+import admin from "firebase-admin";
+import moment from "moment"
+import { readFile } from 'fs/promises';
+
+
+const serviceAccount = JSON.parse(await readFile(new URL('./serviceAccountKey.json', import.meta.url)));
+
+
+
+ admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+
+
+// Get a list of cities from your database
+async function getAllPosts() {
+  const postCollection = await db.collection( 'blogs').get();
+    const postList = postCollection.docs.map(doc => doc.data());
+  return postList;
+}
+
+async function addPost(post) {
+    await db.collection( 'blogs').doc().set(post);
+}
+
+
+export {getAllPosts,addPost}
