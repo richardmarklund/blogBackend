04c783e741a5bb93cfa87f5ffb396e850311194b
Richard Marklund <richard.marklund@gmail.com>
2022-01-28 10:14:47 +0100

    test if the automatic deployment works

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/allPosts", (req, res) => {
     });
 });
 
-app.get("/", (req, res) => {
+app.get("", (req, res) => {
       res.setHeader("Content-Type", "application/json");
       res.send("helloWorld");
 });
