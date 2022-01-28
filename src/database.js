04294b46afa3f64bb4497e4979b1785fc057d76f
Richard Marklund <richard.marklund@gmail.com>
2022-01-28 10:15:07 +0100

    fix for env

--- src/database.js
+++ src/database.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function getAllPosts() {
-  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  const client = new Client({ connectionString: process.env.DATABASEURL });
   await client.connect();
   const res = await client.query('SELECT * FROM public."blogPost"');
   await client.end();
   return res.rows;
 }
 async function addPost(post) {
-  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  const client = new Client({ connectionString: process.env.DATABASEURL });
   await client.connect();
   const res = await client.query(
     `INSERT INTO public."blogPost" (date,topic,body) VALUES ('${post.date}','${post.topic}','${post.body}')`
