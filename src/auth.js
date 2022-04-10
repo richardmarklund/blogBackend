8e8e85e686686ddef0f8c586c56c6141e340e88c
Richard Marklund <richard.marklund@purplescout.se>
2022-04-10 18:14:12 +0200

    add cors

--- src/auth.js
+++ src/auth.js
@@ -10,7 +10,7 @@ const verifyToken = (req, res, next) => {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, process.env.API_TOKEN_SECRET);
+    jwt.verify(token, process.env.API_TOKEN_SECRET);
   } catch (err) {
     console.log(err);
     return res.status(401).send("Invalid Token");
